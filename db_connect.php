f2ffd4727f052112371f8523124a62fd38abc4cb
RicoSuaveDesign <arico@carthage.edu>
2018-03-20 01:27:11 -0500

    Added a function to return the connection

--- db_connect.php
+++ db_connect.php
@@ -4,16 +4,23 @@
  * A class file to connect to database
  */
 class DB_CONNECT {
+
+    
  
+    private $conn = null;
     // constructor
     function __construct() {
         // connecting to database
-        $this->connect();
+       $this->conn =  $this->connect();
+    }
+
+    function showconn(){
+        return $this->conn;
     }
  
     // destructor
-    function __destruct() {
-        // closing db connection
+   function __destruct() {
+       // closing db connection
         $this->close();
     }
  
@@ -25,10 +32,22 @@ class DB_CONNECT {
         require_once __DIR__ . '/db_config.php';
  
         // Connecting to mysql database
-        $con = mysql_connect(DB_SERVER, DB_USER, DB_PASSWORD) or die(mysql_error());
+        $con = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD);
+
+        if (!$con) {
+            echo "Error: Unable to connect to MySQL." . PHP_EOL;
+            echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+            echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+            exit;
+        }
  
         // Selecing database
-        $db = mysql_select_db(DB_DATABASE) or die(mysql_error()) or die(mysql_error());
+        $db = mysqli_select_db($con, DB_DATABASE);
+
+        if (!$db) {
+            echo "Error: Unable to select DB." . PHP_EOL;
+            exit;
+        }
  
         // returing connection cursor
         return $con;
@@ -39,7 +58,7 @@ class DB_CONNECT {
      */
     function close() {
         // closing db connection
-        mysql_close();
+       mysqli_close($this->conn);
     }
  
 }
