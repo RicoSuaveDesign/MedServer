542ace3acbd3245e46e0b414b3de917721ea0c8e
RicoSuaveDesign <arico@carthage.edu>
2018-03-20 01:42:04 -0500

    Added underscores to posts
    A lot of minor details were missing, but most of them have been fixed now so I can rule out serverside code being the problem when testing.

--- create_medicine.php
+++ create_medicine.php
@@ -6,52 +6,54 @@
 $response = array();
 
 //check fields
-if (isset($_POST['user_id']) && isset($_POST['name']) && isset($_POST['medFreqPerTime']) && isset($_POST['medFreqInterval']) && isset($_POST['dosage']) && isset($_POST['unit']) && isset($_POST['expiration']) && isset($_POST['dosesLeft']) && isset($_POST['checkTimes'])){
+if (isset($_POST['user_id']) && isset($_POST['name']) && isset($_POST['medFreqPerTime']) && isset($_POST['medFreqInterval']) && isset($_POST['dosage']) && isset($_POST['unit']) && isset($_POST['expiration']) && isset($_POST['dosesLeft'])){
 
-	$user_id = $POST['user_id'];
-	$name = $POST['name'];
-	$medFreqPerTime = $POST['medFreqPerTime'];
-	$dosage = $POST['dosage'];
-	$unit= $POST['unit'];
-	$expiration = $POST['expiration'];
-	$dosesLeft = $POST['$dosesLeft'];
-	$taken = 0;
+		$user_id = $_POST['user_id'];
+		$name = $_POST['name'];
+		$medFreqPerTime = $_POST['medFreqPerTime'];
+		$dosage = $_POST['dosage'];
+		$unit= $_POST['unit'];
+		$expiration = $_POST['expiration'];
+		$dosesLeft = $_POST['$dosesLeft'];
+		$taken = 0;
 
-	// this should be an array, which will later be iterated over
-	$times = $POST['$checkTimes'];
+		// this should be an array, which will later be iterated over
+		$times = $_POST['$checkTimes'];
 
-	require_once __DIR__ . '/db_connect.php';
+		require_once __DIR__ . '/db_connect.php';
 
-	//connect to database
-	$db = new DB_CONNECT();
+		//connect to database
+		$db = new DB_CONNECT();
 
-	//Prepare so as to prevent sql injection
-	//$stmt = $db->prepare('INSERT INTO Users(name, allergies) VALUES(?, $allergies);
-	//$stmt->execute(array('name' => $name));
-	// But first lets just make it work, then use prepared stmts
-	
-	//insert row, not safe
-	$result = mysql_query("INSERT INTO Medicines(user_id, med_name, medFreqPerTime, medFreqInterval, dosage, unit, expiration, dosesLeft, taken) VALUES('$user_id', '$name', '$medFreqPerTime', '$medFreqInterval', '$dosage', '$unit', '$expiration', '$dosesLeft', '$taken')");
+		//Prepare so as to prevent sql injection
+		//$stmt = $db->prepare('INSERT INTO Users(name, allergies) VALUES(?, $allergies);
+		//$stmt->execute(array('name' => $name));
+		// But first lets just make it work, then use prepared stmts
 
-	//TODO: Drill into timecheck array and add to timecheck table.
+		$con = $db->showconn();
+		
+		//insert row, not safe
+		$result = mysqli_query($con, "INSERT INTO Medicines(user_id, med_name, medFreqPerTime, medFreqInterval, dosage, unit, expiration, dosesLeft, taken) VALUES('$user_id', '$name', '$medFreqPerTime', '$medFreqInterval', '$dosage', '$unit', '$expiration', '$dosesLeft', '$taken')");
 
-	//check if success
-	if ($result) {
-	$response["success"] = 1;
-	$response["message"] = "Medicine created.";
+		//TODO: Drill into timecheck array and add to timecheck table.
 
-	//echo response
-	echo json_encode($response);
+		//check if success
+		if ($result) {
+		$response["success"] = 1;
+		$response["message"] = "Medicine created.";
+
+		//echo response
+		echo json_encode($response);
+	} else {
+
+		$response["success"] = 0;
+		$response["message"] = "Error: Medicine not created";
+
+		echo json_encode($response);
+	}
 } else {
-
 	$response["success"] = 0;
-	$response["message"] = "Error: Medicine not created";
-
-	echo json_encode($response);
-} else {
-	//required field is missing
-	$response["success"] = 0;
-	$response["message"] = "A required field is missing.";
+	$response["message"] = "Error: A field is missing";
 
 	echo json_encode($response);
 }
