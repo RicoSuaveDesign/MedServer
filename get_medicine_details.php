1a5d413e9fd0a757229f6c0b0ae9bc216fa7cafe
RicoSuaveDesign <arico@carthage.edu>
2018-03-20 01:31:54 -0500

    Fixed up some details.

--- get_medicine_details.php
+++ get_medicine_details.php
@@ -11,48 +11,50 @@ $db = new DB_CONNECT();
 if (isset($_GET["mid"])) {
 	$mid = $_GET['mid'];
 
-	$result = mysql_query("SELECT * FROM Medicine WHERE id = $mid");
+	$con = $db->showconn();
+
+	$result = mysqli_query($con, "SELECT * FROM MEDICINES WHERE tag_id = $mid");
 
 	if(!empty($result)) {
 
-	if(mysql_num_rows($result) > 0) {
+			if(mysqli_num_rows($result) > 0) {
 
-		$result = mysql_fetch_array($result);
+				$result = mysqli_fetch_array($result);
 
-		$med = array();
-		$med["mid"] = $result["mid"];
-		$med["name"] = $result["name"];
-		$med["medFreqPerTime"] = $result["medFreqPerTime"];
-		$med["medFreqInterval"] = $result["$medFreqInterval"];
-		$med["dosage"] = $result["dosage"];
-		$med["unit"] = $result["unit"];
-		$med["taken"] = $result["taken"];
+				$med = array();
+				$med["mid"] = $result["tag_id"];
+				$med["name"] = $result["med_name"];
+				$med["medFreqPerTime"] = $result["medFreqPerTime"];
+				$med["medFreqInterval"] = $result["medFreqInterval"];
+				$med["dosage"] = $result["dosage"];
+				$med["unit"] = $result["unit"];
+				$med["taken"] = $result["taken"];
 
-		// ADD CHECK TIME CODE HERE WHEN IS INSERTABLE
+				// ADD CHECK TIME CODE HERE WHEN IS INSERTABLE
 
-		$response["success"] = 1;
+				$response["success"] = 1;
 
-		$response["medicine"] = array();
-		array_push($response["medicine"], $medicine);
+				$response["medicine"] = array();
+				array_push($response["medicine"], $med);
 
-		echo json_encode($response);
-} else {
+				echo json_encode($response);
+		} else {
 
+				$response["success"] = 0;
+				$response["message"] = "Medicine not found.";
+
+				echo json_encode($response);
+			}
+	} else {
 		$response["success"] = 0;
-		$response["message"] = "Medicine not found.";
+		$response["message"] = "An id was sent, and nothing was found.";
 
 		echo json_encode($response);
 	}
 } else {
 	$response["success"] = 0;
-	$response["message"] = "Medicine not found, and no rows returned.";
+	$response["message"] = "An id wasn't sent.";
 
-	echo json_encode($response);
-} else {
-
-	$response["success"] = 0;
-	$response["message"] = "A medicine id wasn't sent by clicking on that medicine.";
-
-	echo json_encode($response);
+	echo json_encode($response);	
 }
 ?>
