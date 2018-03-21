f5d8b1b7c16aa31a2a1732b5e2027e4bc77e5bbd
RicoSuaveDesign <arico@carthage.edu>
2018-03-20 20:07:24 -0500

    Fixed foreign keys
    They were not matching the all caps of the tables they were referring to

--- med_init.sql
+++ med_init.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS USERS
 (user_id int unsigned not null auto_increment, name varchar(100) not null, fprint int, allergies tinyint not null, PRIMARY KEY (user_id));
 
 CREATE TABLE IF NOT EXISTS MEDICINES
-(tag_id int unsigned not null auto_increment, med_name varchar(100) not null, medFreqPerTime int not null, medFreqInterval bigint unsigned, dosage double not null, unit varchar(30), expiration date, dosesLeft int, taken tinyint, user_id int unsigned not null, PRIMARY KEY(tag_id), FOREIGN KEY (user_id) REFERENCES Users(user_id));
+(tag_id int unsigned not null auto_increment, med_name varchar(100) not null, medFreqPerTime int not null, medFreqInterval bigint unsigned, dosage double not null, unit varchar(30), expiration date, dosesLeft int, taken tinyint, user_id int unsigned not null, PRIMARY KEY(tag_id), FOREIGN KEY (user_id) REFERENCES USERS(user_id));
 
 CREATE TABLE IF NOT EXISTS CHECKTIMES
-(time_id int unsigned not null auto_increment, checkTime time not null, tag_id int unsigned not null, PRIMARY KEY (time_id), FOREIGN KEY (tag_id) REFERENCES Medicines(tag_id));
+(time_id int unsigned not null auto_increment, checkTime time not null, tag_id int unsigned not null, PRIMARY KEY (time_id), FOREIGN KEY (tag_id) REFERENCES MEDICINES(tag_id));
